efa1f7ba72b46fef42792ca41b1ef735c20d51ec
Bohdan Drabynko <drabynko.b@gmail.com>
2023-04-11 18:00:29 +0300

    structure built

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 
+import {
+  Navbar,
+  Feed,
+  VideoDetail,
+  ChannelDetail,
+  SearchFeed,
+} from "./components";
+
 const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: "#000" }}></Box>
-    Navbar
+    <Navbar />
     <Routes>
       <Route path="/" exact element={<Feed />} />
+      <Route path="/video/:id" element={<VideoDetail />}></Route>
+      <Route path="/channel/:id" element={<ChannelDetail />}></Route>
+      <Route path="/search/:searchTerm" element={<SearchFeed />}></Route>
     </Routes>
   </BrowserRouter>
 );
